6d5565fbfb18146465480e6913f59e9ea9af9bc4
pantherauncia0406 <pantherauncia0406@gmail.com>
2020-08-06 15:46:17 +0900

    HoT: Initial revision

---
+++ Heads_or_Tails.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int round[5];
+    int i, H=0, T=0;
+
+    for(i = 0; i<5; i++){
+        round[i] = (rand()%2);
+    }
+
+    for(i = 0; i<5; i++){
+        if(round[i] == 1){
+            H++;
+            printf("Round %d: Heads\n", i+1);
+        }
+        else{
+            T++;
+            printf("Round %d: Tails\n", i+1);
+        }
+    }
+    printf("Heads: %d, Tails: %d\n", H, T);
+    
+    return 0;
+}
